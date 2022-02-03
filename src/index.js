aa4a9d3345f3f14e341a192ac49980649db9629d
NikitaShakhovich <7149382@tut.by>
2022-02-03 23:40:16 +0300

    feat: add an association array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function towelSort (matrix) {
-    return matrix.join(',').split(',').map(Number);
+    let result = []
+    if (matrix == undefined || matrix.length === 0) {
+        return result
+    } else {
+        for (let i = 1; i < matrix.length; i++) {
+            if(i%2 === 1) {
+                matrix[i] = matrix[i].reverse();
+            }
+        }
+    }
+
+    result = matrix.join(',').split(',').map(Number);
+    return result
 }
